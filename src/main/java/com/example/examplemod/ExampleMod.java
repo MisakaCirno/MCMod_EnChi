88ea3ba2bc65a49e0b83a8eecba21846c3d7a125
yangkun <yangkun@babeltime.com>
2023-12-29 12:55:03 +0800

    添加注释翻译

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -18,28 +18,35 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
+// 这里的值应该与META-INF/mods.toml文件中的条目匹配
 @Mod("examplemod")
 public class ExampleMod
 {
     // Directly reference a slf4j logger
+    // 直接引用slf4j记录器
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public ExampleMod()
     {
         // Register the setup method for modloading
+        // 注册modloading的setup方法
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
+        // 注册modloading的enqueueIMC方法
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
         // Register the processIMC method for modloading
+        // 注册modloading的processIMC方法
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
 
         // Register ourselves for server and other game events we are interested in
+        // 注册我们自己感兴趣的服务器和其他游戏事件
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event)
     {
         // some preinit code
+        // 一些preinit代码
         LOGGER.info("HELLO FROM PREINIT");
         LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
@@ -47,27 +54,32 @@ public class ExampleMod
     private void enqueueIMC(final InterModEnqueueEvent event)
     {
         // Some example code to dispatch IMC to another mod
+        // 一些示例代码将IMC调度到另一个mod
         InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
     }
 
     private void processIMC(final InterModProcessEvent event)
     {
         // Some example code to receive and process InterModComms from other mods
+        // 一些示例代码来接收和处理来自其他mod的InterModComms
         LOGGER.info("Got IMC {}", event.getIMCStream().
                 map(m->m.messageSupplier().get()).
                 collect(Collectors.toList()));
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
+    // 你可以使用SubscribeEvent并让Event Bus发现要调用的方法
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event)
     {
         // Do something when the server starts
+        // 服务器启动时做一些事情
         LOGGER.info("HELLO from server starting");
     }
 
     // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
     // Event bus for receiving Registry Events)
+    // 您可以使用EventBusSubscriber在包含的类上自动订阅事件（这是订阅MOD事件总线以接收注册表事件）
     @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents
     {
@@ -75,6 +87,7 @@ public class ExampleMod
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent)
         {
             // Register a new block here
+            // 在这里注册一个新的块
             LOGGER.info("HELLO from Register Block");
         }
     }
