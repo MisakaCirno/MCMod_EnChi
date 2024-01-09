4c9247d5da5e5b7379d8298d4b248660a453aa41
MisakaCirno <misakacirno@qq.com>
2024-01-10 00:13:51 +0800

    添加点测试代码

--- src/main/java/com/toolran/mc/enchixiaobinggan/Main.java
+++ src/main/java/com/toolran/mc/enchixiaobinggan/Main.java
@@ -1,6 +1,7 @@
 package com.toolran.mc.enchixiaobinggan;
 
 
+import com.mojang.logging.LogUtils;
 import net.minecraft.Util;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
@@ -8,18 +9,37 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import org.slf4j.Logger;
 
 @Mod(Main.MOD_ID)
 @Mod.EventBusSubscriber
 public class Main {
     public static final String MOD_ID = "enchixiaobinggan";
 
+    // Directly reference a slf4j logger
+    // 直接引用slf4j记录器
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Main() {
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        // 一些preinit代码
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    // 一些测试用的代码
     @SubscribeEvent
     public static void playJoinWorld(PlayerEvent.PlayerLoggedInEvent event) {
         Player player = event.getPlayer();
@@ -61,10 +81,10 @@ public class Main {
     public static void rightClickItem(PlayerInteractEvent.RightClickItem event) {
         Player player = event.getPlayer();
         Level level = player.level;
-        if (!level.isClientSide()){
+        if (!level.isClientSide()) {
             ItemStack itemStack = event.getItemStack();
             Item item = itemStack.getItem();
-            if (item == Items.DIAMOND_SWORD){
+            if (item == Items.DIAMOND_SWORD) {
                 HitResult hitResult = player.pick(20, 0, false);
                 Vec3 location = hitResult.getLocation();
                 player.teleportTo(location.x, location.y, location.z);
